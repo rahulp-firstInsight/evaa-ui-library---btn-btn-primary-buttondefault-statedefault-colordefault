5a9ac48ffbedd669c8a897be56c78efc1db366f9
rahulp-firstInsight <Rahulp@first-insight.com>
2025-09-25 00:31:35 +0530

    Initial commit from your app

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IconClose } from "../IconClose";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.scss";
 
 interface Props {
   label: string;
@@ -21,30 +21,9 @@ export const PrimaryButton = ({
 }: Props): JSX.Element => {
   return (
     <div
-      className={`d-flex align-items-center justify-content-center position-relative overflow-hidden ${className}`}
-      style={{
-        aspectRatio: "3.71",
-        backgroundColor: "#40eccf",
-        border: "none",
-        borderRadius: "8px",
-        padding: "8px 20px",
-        width: "176px",
-        top: "20px",
-        left: "12px",
-      }}
+      className={`d-flex align-items-center justify-content-center btn btn-primary ${className}`}
     >
-      <div
-        className="text-white font-weight-medium"
-        style={{
-          fontFamily: '"Inter", Helvetica',
-          fontSize: "16px",
-          height: "24px",
-          textAlign: "center",
-          width: "86px",
-        }}
-      >
-        {label}
-      </div>
+      <div className="text-white">{label}</div>
       {hasIcon && <IconClose className="icon-close-instance" />}
     </div>
   );
